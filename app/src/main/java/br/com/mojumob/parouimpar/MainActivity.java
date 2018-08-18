fce3a798fa852fe545b612352118402a3077babc
bytecore@gmail.com <mo10128>
2018-08-18 15:06:33 -0300

    Definindo os atributos

--- app/src/main/java/br/com/mojumob/parouimpar/MainActivity.java
+++ app/src/main/java/br/com/mojumob/parouimpar/MainActivity.java
@@ -2,8 +2,17 @@ package br.com.mojumob.parouimpar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Atributos
+    private EditText edtNumero;
+    private Button btnPar, btnImpar;
+    private TextView nComputador, somaTotal, parOuImpar, ganhador;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
